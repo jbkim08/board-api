c449c4edff8d48ed650b2e5be49064920c3ff587
KJB <drv98@naver.com>
2026-07-23 15:30:05 +0900

    CORS 허용(테스트)

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/boards")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*") //CORS 허용
 public class BoardController {
 
     private final BoardService boardService;
